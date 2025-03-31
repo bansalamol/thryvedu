cd84949aa51c236284ef7468de66a1e58ad4b93b
ashutosh-gupta1902 <ashutosh.gupta@maestriainfotech.com>
2025-03-31 20:06:40 +0530

    Route updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'), 
+    path('', views.index, name='index'), 
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -7,31 +7,33 @@ from django.contrib.auth import login
 
 from django.contrib import messages
 
-def home(request):
-    return render(request, 'accounts/home.html')
+
+def index(request):
+    return render(request, "accounts/home.html")
 
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()  # Save the new user
-            messages.success(request, 'Your account has been created successfully!')
-            return redirect('')
+            form.save()
+            messages.success(request, "Your account has been created successfully!")
+            return redirect("")
         else:
-            messages.error(request, 'There was an error during registration.')
+            messages.error(request, "There was an error during registration.")
     else:
         form = UserCreationForm()
-    return render(request, 'accounts/register.html', {'form': form})
+    return render(request, "accounts/register.html", {"form": form})
+
 
 def user_login(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
-            login(request, user) 
-            messages.success(request, 'Your account has been loggedin successfully!')
+            login(request, user)
+            messages.success(request, "Your account has been loggedin successfully!")
     else:
         form = AuthenticationForm()
 
-    return render(request, 'accounts/login.html', {'form': form})
+    return render(request, "accounts/login.html", {"form": form})
